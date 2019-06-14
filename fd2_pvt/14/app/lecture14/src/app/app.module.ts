cd7e83f33734a7a53f976524d69bb88f9d0ddac0
paawel <rsk.pawel@gmail.com>
2019-06-14 22:12:20 +0300

    update

--- fd2_pvt/14/app/lecture14/src/app/app.module.ts
+++ fd2_pvt/14/app/lecture14/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Slide1Component } from './slide1/slide1.component';
+import { Slide2Component } from './slide2/slide2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Slide1Component
+    Slide1Component,
+    Slide2Component
   ],
   imports: [
     BrowserModule
